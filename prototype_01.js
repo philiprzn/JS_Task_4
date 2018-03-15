701a5fdcd2b8454c203f93db81e7cfed18361e6d
philiprzn <philip_bmx11@yahoo.com>
2018-03-15 22:47:03 +0300

    proto 01.1

--- prototype_01.js
+++ prototype_01.js
@@ -1,14 +1,25 @@
 function checkProto(propName, obj) {
     var objProto = obj.__proto__;
+
+    for(var prop in obj){
+        if (obj.hasOwnProperty(prop) && prop === propName) return false;
+    }
+
     for (var key in objProto){
         if (key === propName) return objProto[key];
     }
+
+    return false;
 };
 
 
 var obj = {
     a : 1,
-    b : 2
+    b : 2,
+    d : {
+        x : 9,
+        y : false
+    }
 };
 var newObj = Object.create(obj);
 newObj.c = 3;
@@ -17,4 +28,5 @@ newObj.c = 3;
 
 // checkProto('b', newObj);
 
-console.log(checkProto('b', newObj));
+console.log(checkProto('d', newObj));
+console.log(checkProto('c', newObj));
