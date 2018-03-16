59dea761f567957ba0306cb7a5ffc73132982220
Philip Maksiutov <philip_bmx11@yahoo.com>
2018-03-16 22:52:33 +0300

    js task 6

--- js_task4_06.js
+++ js_task4_06.js
@@ -1,5 +1,6 @@
 function toCamelCase(str) {
     lowStr = str.toLowerCase();
+
     lowStr = lowStr.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
         return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
     }).replace(/\s+/g, '');
