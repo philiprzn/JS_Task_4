8a88e3925be6182ad470f6e1ed9e2c4be578e042
Philip Maksiutov <philip_bmx11@yahoo.com>
2018-03-16 22:46:27 +0300

    js task 6

---
+++ js_task4_06.js
@@ -0,0 +1,11 @@
+function toCamelCase(str) {
+    lowStr = str.toLowerCase();
+    lowStr = lowStr.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+        return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+    }).replace(/\s+/g, '');
+
+    return  lowStr;
+}
+
+console.log(toCamelCase('Great coDe Current String'));
+console.log(toCamelCase('Great coDe CurRRent sTring'));
