a6d2b31f5e3836d03bc9ccd3f68d131d2efccc41
Philip Maksiutov <philip_bmx11@yahoo.com>
2018-03-16 15:05:20 +0300

    js task 1

---
+++ js_task4_01.js
@@ -0,0 +1,8 @@
+function trimSpasces(string) {
+
+    // return string.replace(/^\s+|\s+$/g, "");
+    // return string.replace(/(^\s+|\s+$)/g, '');
+    return string.trim();
+}
+
+console.log(trimSpasces('  Метод trim() возвращает строку с вырезанными пробельными символами с её концов.   '));
